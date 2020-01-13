b0e27764ca024bb9e77085bd266ec98e62661a75
arslanah99 <arah09@hotmail.com>
2020-01-12 21:50:28 -0700

    implemented surveyjs into app

--- src/components/SurveyEx.js
+++ src/components/SurveyEx.js
@@ -1,12 +1,42 @@
 // src/components/NavBar.js
-
 import React from "react";
+import * as Survey from "survey-react";
+import "survey-react/survey.css";
 
 const SurveyEx = () => {
+  var surveyJSON = {
+    pages: [
+      {
+        name: "page2",
+        elements: [
+          {
+            type: "rating",
+            name: "question2",
+            title: "Whats the biggest number here?"
+          },
+          {
+            type: "rating",
+            name: "question3",
+            title: "Whats the  smallest number here?"
+          },
+          {
+            type: "rating",
+            name: "question4",
+            title: "Whats the most middle number?"
+          }
+        ]
+      }
+    ]
+  };
+
+  function sendDataToServer(survey) {
+    //send Ajax request to your web server.
+    alert("The results are:" + JSON.stringify(survey.data));
+  }
 
   return (
     <div>
-      <h1>HELLO</h1>
+      <Survey.Survey json={surveyJSON} onComplete={sendDataToServer} />
     </div>
   );
 };
